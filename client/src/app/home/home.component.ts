558fa9223a9d2263073df227181a8864e8cf81f5
Wout De Tollenaere <woutdt@icloud.com>
2018-09-14 21:34:19 +0200

    GET opgezet in client

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  res : any;
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    return this.http.get("http://localhost:3000/find")
+      .subscribe(data => {
+        this.res = data;
+        console.log(this.res);
+      });
+  };
+
+}
