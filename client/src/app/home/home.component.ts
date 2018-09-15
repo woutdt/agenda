789af1927ed127c99756beb7341b9435e617b5ce
Wout De Tollenaere <woutdt@icloud.com>
2018-09-15 17:51:00 +0200

    started client

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -19,6 +19,10 @@ export class HomeComponent implements OnInit {
     this.getData();
   }
 
+  alertId(id :any) {
+    alert(id);
+  };
+
   getData() {
     return this.http.get("http://localhost:3000/find")
       .subscribe(data => {
